d1f958eea757667775735647e9f246b7bb33f369
Isidro Torregrosa <isistorre95@gmail.com>
2020-05-08 08:58:15 +0200

    format

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import './app.css';
 
 function App() {
   return (
-
     <nav className='app__navegacion'>
       <ul className='app__navegacion__lista'>
-       
-    <MenuItem text='Tareas' link='#'></MenuItem>
-    <MenuItem text='Nueva Tarea' link='#'></MenuItem>
-      
+        <MenuItem text='Tareas' link='#'></MenuItem>
+        <MenuItem text='Nueva Tarea' link='#'></MenuItem>
       </ul>
-
-
     </nav>
   );
 }
--- src/menuItem.js
+++ src/menuItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './menuItem.css';
 
-function MenuItem(props){
-return(
-<li className='menu-item'>
- <a href={props.link} className='menu-item__link'>{props.text}</a>
-</li>
-)
+function MenuItem(props) {
+    return (
+        <li className='menu-item'>
+            <a href={props.link} className='menu-item__link'>{props.text}</a>
+        </li>
+    );
 }
 
 export default MenuItem;

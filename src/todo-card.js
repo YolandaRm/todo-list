c93116c2b47f0e5c323bcf4494847cb745596f33
ark4noid <ark4no07@gmail.com>
2020-05-08 11:38:15 +0200

    todo-card

--- src/todo-card.js
+++ src/todo-card.js
@@ -12,26 +12,31 @@ export default class TodoCard extends React.Component {
         // con fetch tienes que hacer un delete
         // cuando se pulse el boton de borrar tarea
     }
-    _complete = (newStatus) => {
+
+    _completado = (newStatus) => {
         // con fetch un put para actualizar el status de la tarea
         // cuando se pulse el boton de completar, descartar
     }
+
     _pendiente = () => {
 
     }
+
     _descartado = () => {
 
     }
+
     _getClassName() {
         // en funcion del estado de la tarea tiene que devolver una clase de css
         // --completada, --pendiente, --descartada
     }
+
     render() {
         // devolver un html que pinte this.props.item.text y los 3 botones
         return (
-        <Button onClick={_compplete}>Completado</Button>
-        <Button onClick={_discard}>Pendiente</Button>
-        <Button onClick={_descartado}>Descartado</Button>
+        <Button onClick={this._completado}>Completado</Button>
+        <Button onClick={this._pendiente}>Pendiente</Button>
+        <Button onClick={this._descartado}>Descartado</Button>
         );
     }
 
